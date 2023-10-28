b8b4620e9b26982dbdd89e3e154a9b9d32c66950
Kevin Garner <kevinleegarner@gmail.com>
2023-10-27 23:13:16 -0500

    🤣 this is what it's like to be in a hurry

--- scripts/module.mjs
+++ scripts/module.mjs
@@ -1,6 +1,6 @@
 import CONSTANTS from "./constants.mjs";
 import replaceAll from "./helpers/replaceAll.mjs";
-import replace from "./helpers/replaceAll.mjs";
+import replace from "./helpers/replace.mjs";
 
 Hooks.on("ready", function () {
   Handlebars.registerHelper(`${CONSTANTS.MODULE_ID}-replace`, replace);
