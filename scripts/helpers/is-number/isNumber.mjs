21f71bb4d4a050616e7ab33628c48ce493b3faa0
Kevin Garner <kevin.garner@ssigroup.com>
2023-12-04 16:46:07 -0600

    Added math functions.

---
+++ scripts/helpers/is-number/isNumber.mjs
@@ -0,0 +1,9 @@
+export default function isNumber(num) {
+  if (typeof num === 'number') {
+    return num - num === 0;
+  }
+  if (typeof num === 'string' && num.trim() !== '') {
+    return Number.isFinite ? Number.isFinite(+num) : isFinite(+num);
+  }
+  return false;
+}
