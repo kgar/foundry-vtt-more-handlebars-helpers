61ab5a540972ebeb75287dcefd6ebb5f36cea39b
Kevin Garner <kevin.garner@ssigroup.com>
2024-04-15 11:15:06 -0500

    Added gt, gte, lt, lte.
    Documented them.

--- scripts/module.mjs
+++ scripts/module.mjs
@@ -1,4 +1,8 @@
 import CONSTANTS from './constants.mjs';
+import gt from './helpers/gt.mjs';
+import gte from './helpers/gte.mjs';
+import lt from './helpers/lt.mjs';
+import lte from './helpers/lte.mjs';
 import replaceAll from './helpers/replaceAll.mjs';
 import replace from './helpers/replace.mjs';
 import split from './helpers/split.mjs';
@@ -14,6 +18,10 @@ Hooks.on('ready', function () {
   Handlebars.registerHelper(`${CONSTANTS.MODULE_ID}-ceil`, math.ceil);
   Handlebars.registerHelper(`${CONSTANTS.MODULE_ID}-divide`, math.divide);
   Handlebars.registerHelper(`${CONSTANTS.MODULE_ID}-floor`, math.floor);
+  Handlebars.registerHelper(`${CONSTANTS.MODULE_ID}-gt`, gt);
+  Handlebars.registerHelper(`${CONSTANTS.MODULE_ID}-gte`, gte);
+  Handlebars.registerHelper(`${CONSTANTS.MODULE_ID}-lt`, lt);
+  Handlebars.registerHelper(`${CONSTANTS.MODULE_ID}-lte`, lte);
   Handlebars.registerHelper(`${CONSTANTS.MODULE_ID}-modulo`, math.modulo);
   Handlebars.registerHelper(`${CONSTANTS.MODULE_ID}-multiply`, math.multiply);
   Handlebars.registerHelper(`${CONSTANTS.MODULE_ID}-random`, math.random);
